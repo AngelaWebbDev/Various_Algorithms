5e480cd6a0c39daf72cf1e3e7dc38b084d6fd1c9
Angela Webb <angelawebb.dev@gmail.com>
2024-02-20 10:44:59 -0500

    adds printintsandsum0to255 in py

--- python.py
+++ python.py
@@ -5,3 +5,11 @@ def print1to255():
         
 #print1to255()
 
+#print integers from 0 to 255, and with each integer print the sum so far
+def printIntsAndSum0To255():
+    print('printIntsAndSum0To255 in python: ')
+    sum = 0
+    for i in range(1,256):
+        sum+=i
+        print(i, " ", sum)
+printIntsAndSum0To255()
